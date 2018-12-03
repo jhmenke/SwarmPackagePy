9351e24da5488c9b047fb525721613de7d0246c5
Jan-Hendrik Menke <jan-hendrik.menke@uni-kassel.de>
2018-12-03 15:13:22 +0100

    cso with larger levy_fly

--- SwarmPackagePy/cso.py
+++ SwarmPackagePy/cso.py
@@ -10,11 +10,10 @@ class cso(intelligence.sw):
     Cuckoo Search Optimization
     """
 
-    def __init__(self, n, function, lb, ub, dimension, iteration, pa=0.25,
-                 nest=100, initfunc=None):
+    def __init__(self, n, eval_func, lb, ub, dimension, iteration, pa=0.25, nest=100, initfunc=None):
         """
         :param n: number of agents
-        :param function: test function
+        :param eval_func: test function
         :param lb: lower bound for the function variables
         :param ub: upper bound for the function variables
         :param dimension: space dimension
@@ -29,19 +28,17 @@ class cso(intelligence.sw):
         self.__Nests = []
 
         beta = 3 / 2
-        sigma = (gamma(1 + beta) * sin(pi * beta / 2) / (
-            gamma((1 + beta) / 2) * beta *
-            2 ** ((beta - 1) / 2))) ** (1 / beta)
-        u = np.array([normalvariate(0, 1) for k in range(dimension)]) * sigma
-        v = np.array([normalvariate(0, 1) for k in range(dimension)])
+        sigma = (gamma(1 + beta) * sin(pi * beta / 2) / (gamma((1 + beta) / 2) * beta *
+                                                         2 ** ((beta - 1) / 2))) ** (1 / beta)
+        u = np.array([normalvariate(0, 1) for _ in range(dimension)]) * sigma
+        v = np.array([normalvariate(0, 1) for _ in range(dimension)])
         step = u / abs(v) ** (1 / beta)
 
         if not callable(initfunc):
             initfunc = np.random.uniform
         self.__agents = initfunc(lb, ub, (n, dimension))
         self.__nests = initfunc(lb, ub, (nest, dimension))
-        Pbest = self.__nests[np.array([function(x)
-                                       for x in self.__nests]).argmin()]
+        Pbest = self.__nests[np.array([eval_func(x) for x in self.__nests]).argmin()]
         Gbest = Pbest[:]
         self._points(self.__agents)
 
@@ -49,12 +46,12 @@ class cso(intelligence.sw):
 
             for i in self.__agents:
                 val = randint(0, nest - 1)
-                if function(i) < function(self.__nests[val]):
+                if eval_func(i) < eval_func(self.__nests[val]):
                     self.__nests[val] = i
 
-            fnests = [(function(self.__nests[i]), i) for i in range(nest)]
+            fnests = [(eval_func(self.__nests[i]), i) for i in range(nest)]
             fnests.sort()
-            fcuckoos = [(function(self.__agents[i]), i) for i in range(n)]
+            fcuckoos = [(eval_func(self.__agents[i]), i) for i in range(n)]
             fcuckoos.sort(reverse=True)
 
             nworst = nest // 2
@@ -64,26 +61,21 @@ class cso(intelligence.sw):
                 if random() < pa:
                     self.__nests[i] = initfunc(lb, ub, (1, dimension))
 
-            if nest > n:
-                mworst = n
-            else:
-                mworst = nest
+            mworst = n if nest > n else nest
 
             for i in range(mworst):
-
                 if fnests[i][0] < fcuckoos[i][0]:
                     self.__agents[fcuckoos[i][1]] = self.__nests[fnests[i][1]]
 
             self.__nests = np.clip(self.__nests, lb, ub)
-            self.__Levyfly(step, Pbest, n, dimension)
+            self.__levy_fly(step, Pbest, n, dimension)
             self.__agents = np.clip(self.__agents, lb, ub)
             self._points(self.__agents)
             self.__nest()
 
-            Pbest = self.__nests[np.array([function(x)
-                                        for x in self.__nests]).argmin()]
+            Pbest = self.__nests[np.array([eval_func(x) for x in self.__nests]).argmin()]
 
-            if function(Pbest) < function(Gbest):
+            if eval_func(Pbest) < eval_func(Gbest):
                 Gbest = Pbest[:]
 
         self._set_Gbest(Gbest)
@@ -91,14 +83,12 @@ class cso(intelligence.sw):
     def __nest(self):
         self.__Nests.append([list(i) for i in self.__nests])
 
-    def __Levyfly(self, step, Pbest, n, dimension):
+    def __levy_fly(self, step, Pbest, n, dimension):
 
         for i in range(n):
-            stepsize = 0.2 * step * (self.__agents[i] - Pbest)
-            self.__agents[i] += stepsize * np.array([normalvariate(0, 1)
-                                                    for k in range(dimension)])
+            stepsize = 0.5 * step * (self.__agents[i] - Pbest)
+            self.__agents[i] += stepsize * np.array([normalvariate(0, 1) for _ in range(dimension)])
 
     def get_nests(self):
         """Return a history of cuckoos nests (return type: list)"""
-
         return self.__Nests
